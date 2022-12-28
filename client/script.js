f11f4229b9e69b50374e41ae87abeed3c6190678
Monoget <monoget2@gmail.com>
2022-12-28 12:35:11 +0600

    Update File

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-ngt-tech.onrender.com/', {
+    const response = await fetch('https://ai-ngt-tech-ysc0.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
